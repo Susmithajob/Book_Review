81d9a48ca6e230281ffbf131c74d329806541b45
Susmitha <susmithajob93@gmail.com>
2024-10-14 16:39:57 +0530

    Updated API URLs to Render

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ const PORT=process.env.PORT || 5000
 
 
 app.use(cors({
-    origin: "*",
+    origin: "https://know-more.onrender.com",
     methods: ["GET", "POST", "DELETE"],
     allowedHeaders: ["Content-Type"],
     credentials: true
